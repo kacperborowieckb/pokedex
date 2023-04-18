3687e95dfb1aa4f78f30e8426ffdd279e036b559
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-04-18 21:32:07 +0200

    handle errors

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,18 +12,26 @@ const Home = () => {
 
   useEffect(() => {
     const fetchSinglePokemon = async (url) => {
-      const response = await axios.get(url);
-      return response.data;
+      try {
+        const response = await axios.get(url);
+        return response.data;
+      } catch (err) {
+        console.log(`Error: ${err.message}`);
+      }
     };
 
     const fetchPokemons = async () => {
-      const response = await axios.get(API_URL);
-      const pokemons = [];
-      for (let item of response.data.results) {
-        const pokemonData = await fetchSinglePokemon(item.url);
-        pokemons.push(pokemonData);
+      try {
+        const response = await axios.get(API_URL);
+        const pokemons = [];
+        for (let item of response.data.results) {
+          const pokemonData = await fetchSinglePokemon(item.url);
+          pokemons.push(pokemonData);
+        }
+        setPokemons(pokemons);
+      } catch (err) {
+        console.log(`Error: ${err.message}`);
       }
-      setPokemons(pokemons);
     };
 
     fetchPokemons();
