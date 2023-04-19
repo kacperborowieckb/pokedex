e7930149139deeea109fe8a6e81ea1ad408764da
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-04-19 16:40:19 +0200

    scrollbar design + beter search

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,12 @@
 import '../../styles/main.scss';
 import SearchResult from '../SearchResult/SearchResult';
 import './nav.scss';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Nav = ({ pokemons }) => {
   const [search, setSearch] = useState('');
   const [searchResult, setSearchResult] = useState([]);
+  const [isFocused, setIsFocused] = useState(false);
 
   useEffect(() => {
     const filteredPokemons = pokemons.filter((pokemon) =>
@@ -34,9 +35,11 @@ const Nav = ({ pokemons }) => {
           onChange={(e) => {
             setSearch(e.target.value);
           }}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
         />
       </form>
-      {search.length > 0 && (
+      {search.length > 0 && isFocused && (
         <div className="nav__results">
           {searchResult.length > 0 ? (
             searchResult.map((pokemon, i) => (
