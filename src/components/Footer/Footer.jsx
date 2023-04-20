8d0ecec8578648fb3d74f175826656c0459d5878
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-04-20 08:53:24 +0200

    footer design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,23 @@ import '../../styles/main.scss';
 import './footer.scss';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <h3 className="footer__header">Pokedex by Kacper Borowiec</h3>
+      <ul className="footer__list">
+        <li className="footer__list-item">
+          <a href="https://github.com/kacperborowieckb" target="_blank">
+            GitHub
+          </a>
+        </li>
+        <li className="footer__list-item">
+          <a href="https://github.com/kacperborowieckb/pokedex" target="_blank">
+            Repo
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
